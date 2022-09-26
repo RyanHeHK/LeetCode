28f56c46a2f2375af2bc8d1442dd36a8b6358c55
RyanHeHK <helei.cnhk@gmail.com>
2022-09-26 15:54:43 +0800

    LeetCode168

---
+++ LeetCode168_Excel表列名称.go
@@ -0,0 +1,13 @@
+package main
+
+func convertToTitle(columnNumber int) string {
+	m := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+	curStr := ""
+	for columnNumber > 0 {
+		columnNumber--
+		yu := columnNumber % 26
+		curStr = m[yu] + curStr
+		columnNumber = columnNumber / 26
+	}
+	return curStr
+}
