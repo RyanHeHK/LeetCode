cdf07fae50025f81591f1444f894c080425bdf0f
RyanHeHK <helei.cnhk@gmail.com>
2022-10-11 18:14:07 +0800

    LeetCode

---
+++ LeetCode1790_仅执行一次字符串交换能否使两个字符串相等.go
@@ -0,0 +1,24 @@
+package main
+
+func areAlmostEqual(s1 string, s2 string) bool {
+	count := 0
+	index := -1
+	for i, _ := range s1 {
+		if s1[i] != s2[i] {
+			if count == 2 {
+				return false
+			}
+			if index != -1 {
+				if s1[i] != s2[index] || s1[index] != s2[i] {
+					return false
+				}
+			}
+			index = i
+			count++
+		}
+	}
+	if count == 1 {
+		return false
+	}
+	return true
+}
